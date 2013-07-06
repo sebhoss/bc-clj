c5457733ff6e695f1012faf96489898ceca641a1
Sebastian Hoß <mail@shoss.de>
2013-07-06 18:33:38 +0200

    Added remaining Digest implementations.
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- src/main/clojure/com/github/sebhoss/bouncycastle/digest.clj
+++ src/main/clojure/com/github/sebhoss/bouncycastle/digest.clj
@@ -1,19 +1,30 @@
 (ns com.github.sebhoss.bouncycastle.digest
-  (:import (org.bouncycastle.crypto.digests MD2Digest
+  (:import (org.bouncycastle.crypto.digests GOST3411Digest
+                                            MD2Digest
                                             MD4Digest
                                             MD5Digest
+                                            RIPEMD128Digest
+                                            RIPEMD160Digest
+                                            RIPEMD256Digest
+                                            RIPEMD320Digest
                                             SHA1Digest
                                             SHA224Digest
                                             SHA256Digest
                                             SHA384Digest
                                             SHA3Digest
-                                            SHA512Digest)))
+                                            SHA512Digest
+                                            TigerDigest
+                                            WhirlpoolDigest)
+           org.bouncycastle.crypto.Digest))
 
 (defn- digest [digest input]
   (.update digest input 0 (alength input))
   (let [result (byte-array (.getDigestSize digest))]
       (.doFinal digest result 0)
       result))
+
+(defn gost3411 [bytes]
+  (digest (GOST3411Digest.) bytes))
 
 (defn md2 [bytes]
   (digest (MD2Digest.) bytes))
@@ -23,6 +34,18 @@
 
 (defn md5 [bytes]
   (digest (MD5Digest.) bytes))
+
+(defn ripemd128 [bytes]
+  (digest (RIPEMD128Digest.) bytes))
+
+(defn ripemd160 [bytes]
+  (digest (RIPEMD160Digest.) bytes))
+
+(defn ripemd256 [bytes]
+  (digest (RIPEMD256Digest.) bytes))
+
+(defn ripemd320 [bytes]
+  (digest (RIPEMD320Digest.) bytes))
 
 (defn sha1 [bytes]
   (digest (SHA1Digest.) bytes))
@@ -41,3 +64,9 @@
 
 (defn sha512 [bytes]
   (digest (SHA512Digest.) bytes))
+
+(defn tiger [bytes]
+  (digest (TigerDigest.) bytes))
+
+(defn whirlpool [bytes]
+  (digest (WhirlpoolDigest.) bytes))
