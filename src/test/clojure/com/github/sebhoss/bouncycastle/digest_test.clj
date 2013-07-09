ba716f80687a70755e8ced2a7f42a79daf0d7e97
Sebastian Hoß <mail@shoss.de>
2013-07-09 22:07:56 +0200

    Added license header
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- src/test/clojure/com/github/sebhoss/bouncycastle/digest_test.clj
+++ src/test/clojure/com/github/sebhoss/bouncycastle/digest_test.clj
@@ -1,3 +1,7 @@
+;   This program is free software. It comes without any warranty, to the extent permitted by applicable law.
+;   You can redistribute it and/or modify it under the terms of the Do What The Fuck You Want To Public
+;   License, Version 2, as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+
 (ns com.github.sebhoss.bouncycastle.digest-test
   (:require [com.github.sebhoss.bouncycastle.digest :refer :all]
             [clojure.test :refer [deftest are]]))
