af526aa8843df534f9a88427bc7642622e7aba6c
Sebastian Hoß <mail@shoss.de>
2013-09-07 20:52:03 +0200

    Use latest bootstrap
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- src/bootstrap/clojure/bootstrap.clj
+++ src/bootstrap/clojure/bootstrap.clj
@@ -1,28 +1,18 @@
 ;
-; This program is free software. It comes without any warranty, to
-; the extent permitted by applicable law. You can redistribute it
-; and/or modify it under the terms of the Do What The Fuck You Want
-; To Public License, Version 2, as published by Sam Hocevar. See
-; http://www.wtfpl.net/ for more details.
+; Copyright © 2013 Sebastian Hoß <mail@shoss.de>
+; This work is free. You can redistribute it and/or modify it under the
+; terms of the Do What The Fuck You Want To Public License, Version 2,
+; as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
 ;
 
+(require '[bootstrap.repl :refer :all])
+
 ; Load project namespaces
-(require '(bouncycastle [digest :as digest]
-                                           [digest-test :as digest-test]))
+(load-ns-in-dir-aliased "src/main/clojure")
+(load-ns-in-dir-aliased "src/test/clojure")
 
 ; 'clojure.test', 'clojure.repl' and 'clojure.tools.namespace.repl' support
-(require '[clojure.tools.namespace.repl :refer :all])
-(require '[clojure.repl :refer :all])
-(require '[clojure.test :refer :all])
+(load-helpers)
 
-; Call (rat) to run all tests, or (rat "math") to run only tests in that namespace
-(defn rat
-  "[R]uns-[A]ll-[T]ests inside the whole project or in a separate namespace.
-
-   Examples:
-     * (rat)          - Run all tests in all project namespaces
-     * (rat \"digest\")   - Run all tests in the digest/-namespace"
-  ([] (run-all-tests #"bouncycastle.*-test"))
-  ([namespace] (run-all-tests
-                 (re-pattern (format "bouncycastle.%s-test"
-                                     namespace)))))
+; Call (rat) to run all tests, or (rat "digest") to run only tests in that namespace
+(def rat (test-shortcut #"bouncycastle.*-test"))
